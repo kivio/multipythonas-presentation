8310438cf5f3f5144f9db523ba87e6d75b68ba90
Marcin Karkocha <marcin.karkocha@polidea.pl>
2013-09-16 12:17:41 +0200

    finish presentation today

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ $(function(){
   Mousetrap.bind('l', function(){
       $("#agenda ol li[class!='show']:first").slideDown().addClass("show");
   });
+    Mousetrap.bind('p', function(){
+        $("#paranoja ul li[class!='show']:first").fadeIn(2500).addClass("show");
+    });
 });
